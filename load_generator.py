87cb338d98b94488f4495a59972fd1faa410d05f
Abhinav Nagpal <abhinavnagpal12@gmail.com>
2020-07-31 14:25:08 +0530

    Update load_generator.py

--- load_generator.py
+++ load_generator.py
@@ -6,18 +6,25 @@ import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def load_generator(hyperparameters, train, val, test):
+def load_generator(hyperparameters, train, val, test, attribute_id=None):
     
     # image_id = '000014.jpg'
     image_path = './dataset/celeba-dataset/'
     train_data_gen = ImageDataGenerator(rescale=1/255.0)
     test_data_gen = ImageDataGenerator(rescale=1/255.0)
     
+    if attribute_id==None:
+        targets = hyperparameters['targets'][1:]
+        mode='multi_output'
+    else:
+        targets = hyperparameters['targets'][attribute_id+1]
+        mode='raw'
+    
     train_gen = train_data_gen.flow_from_dataframe(dataframe = train, 
                                      directory=image_path, 
                                      x_col = 'image_id', 
-                                     y_col=hyperparameters['targets'][1:], 
-                                     class_mode = 'multi_output',
+                                     y_col=targets, 
+                                     class_mode = mode,
                                      target_size=(hyperparameters['height'], hyperparameters['width']), 
                                      batch_size = hyperparameters['batch_size']
                                      )
@@ -25,18 +32,18 @@ def load_generator(hyperparameters, train, val, test):
     val_gen = test_data_gen.flow_from_dataframe(dataframe = val, 
                                      directory=image_path, 
                                      x_col = 'image_id', 
-                                     y_col=hyperparameters['targets'][1:], 
-                                     class_mode = 'multi_output',
-                                     target_size=(hyperparameters['height'], hyperparameters['width']), batch_size = hyperparameters['batch_size'], 
+                                     y_col=targets, 
+                                     class_mode = mode,
+                                     target_size=(hyperparameters['height'], hyperparameters['width']),
+                                     batch_size = hyperparameters['batch_size']
                                      )
 
     test_gen = test_data_gen.flow_from_dataframe(dataframe = test, 
                                      directory=image_path, 
                                      x_col = 'image_id', 
-                                     y_col=hyperparameters['targets'][1:], 
-                                     class_mode = 'multi_output',
-                                     target_size=(hyperparameters['height'], hyperparameters['width']),batch_size = hyperparameters['batch_size'])
+                                     y_col=targets, 
+                                     class_mode = mode,
+                                     target_size=(hyperparameters['height'], hyperparameters['width']), 
+                                     batch_size = hyperparameters['batch_size'])
     
     return train_gen, val_gen, test_gen
-
-
